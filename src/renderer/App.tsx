c3d34333ccb2efeff39ecb774e7ba97964c50150
GaleArt <GaleguzovAA@gmail.com>
2022-04-24 22:37:12 +0300

    edit menu

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -6,11 +6,7 @@ import { Home } from '../pages/home/home';
 import { Calendar } from '../pages/calendar/calendar';
 import { Todo } from '../pages/todo/todo';
 import { Reminder } from '../pages/reminder/reminder';
-import { Archive } from '../pages/archive/archive';
-import { Workers } from '../pages/workers/workers';
-import { CheckList } from '../pages/checklist/checklist';
 import { Calculators } from '../pages/calculators/calculators';
-import { Admin } from '../pages/admin/admin';
 
 export default function App() {
   return (
@@ -20,22 +16,14 @@ export default function App() {
         <Link to="/calendar"><img src="./calendar.png"/></Link>
         <Link to="/todo"><img src="./todo.png"/></Link>
         <Link to="/reminder"><img src="./reminder.png"/></Link>
-        <Link to="/archive"><img src="./archive.png"/></Link>
-        <Link to="/workers"><img src="./workers.png"/></Link>
-        <Link to="/checklist"><img src="./checklist.png"/></Link>
         <Link to="/calculators"><img src="./calculators.png"/></Link>
-        <Link to="/admin"><img src="./admin.png"/></Link>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="calendar" element={<Calendar />} />
         <Route path="todo" element={<Todo />} />
         <Route path="reminder" element={<Reminder />} />
-        <Route path="archive" element={<Archive />} />
-        <Route path="workers" element={<Workers />} />
         <Route path="calculators" element={<Calculators />} />
-        <Route path="checklist" element={<CheckList />} />
-        <Route path="admin" element={<Admin />} />
       </Routes>
     <div class="scene">
   	  <div class="space">
